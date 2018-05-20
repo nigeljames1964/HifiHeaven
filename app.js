285c48b09c49aef15560f0ae437168fe5a419b95
nigel <nigeljames14@outlook.com>
2018-05-20 12:10:15 +0100

    hifiheaven initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+console.log('one');
+/*document.getElementsByClassName("buybtn").​addEventListener("click", function() {
+    addToCart(name, price);
+});
+console.log('two');
+
+function addToCart(name, price) {
+    alert('Item added to cart');
+}*/
+console.log('three');
